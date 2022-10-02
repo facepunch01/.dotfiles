2a5911e14d2c06f7404d43ef51eda32dee30ae8b
Jake Hackl <jakehackl@gmail.com>
2022-10-02 12:56:39 -0500

    general improvements

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 (set-face-attribute 'default nil :family "JetBrainsMono NF" :height 120)
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -14,6 +15,16 @@
 (straight-use-package 'use-package)
 (require 'bind-key)
 (setq native-comp-async-report-warnings-errors 'nil)
+(setq straight-use-package-by-default '1)
+(setq inhibit-startup-message t)
+
+
+(use-package flyspell
+  :hook org-mode-hook
+  :config
+  (setq ispell-list-command "--list")
+  (setq ispell-program-name "aspell")
+        ispell-extra-args "--sug-mode=ultra" "--lang=en_US")
 
 
 (use-package projectile
@@ -22,10 +33,6 @@
   (projectile-mode +1)
   :bind (:map projectile-mode-map
               ("C-x p" . projectile-command-map)))
-
-
-(setq straight-use-package-by-default '1)
-(setq inhibit-startup-message t)
 
 
 (scroll-bar-mode -1)
@@ -54,22 +61,31 @@
 
 (setq modus-themes-mode-line '(borderless))
 (load-theme 'modus-operandi t)
-(display-line-numbers-mode)
+(global-display-line-numbers-mode)
 (setq column-number-mode t)
 
 
 (use-package all-the-icons
   :if (display-graphic-p)
   :defer)
-
 (recentf-mode 1)
-
+(setq evil-want-keybinding nil)
 (use-package goto-chg)
 (use-package evil
   :after goto-chg
   :config
   (evil-mode 1))
-
+(use-package evil-collection
+  :init
+  (evil-collection-init))
+(use-package evil-org
+  :ensure t
+  :after org
+  :hook (org-mode . (lambda () evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-set-key-theme '(navigation insert textobjects additional calendar))
+  (evil-org-agenda-set-keys))
 (use-package vertico
   :bind (:map vertico-map
 	 ("C-j" . vertico-next)
@@ -92,13 +108,52 @@
   (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
   :init
   (marginalia-mode))
+(use-package corfu
+  ;; Optional customizations
+  ;; :custom
+  ;;(corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-echo-documentation nil) ;; Disable documentation in the echo area
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.
+  ;; This is recommended since Dabbrev can be used globally (M-/).
+  ;; See also `corfu-excluded-modes'.
+  :init
+  (global-corfu-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; TAB cycle if there are only few candidates
+  (setq completion-cycle-threshold 3)
+
+  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (setq tab-always-indent 'complete))
 
 
 (setq
  ;; Edit settings
  org-auto-align-tags nil
  org-tags-column 0
- org-catch-invisible-edits 'show-and-error
+ org-fold-catch-invisible-edits 'show-and-error
  org-special-ctrl-a/e t
  org-insert-heading-respect-content t
 
@@ -124,7 +179,8 @@
 
 (use-package org
   :config
-   (setq org-startup-indented t)
+  (setq org-startup-indented t)
+  (setq org-return-follows-link t)
   :mode ("\\.org\\'" . org-mode))
 (use-package org-contrib
   :after org)
@@ -141,5 +197,5 @@
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n i" . org-roam-node-insert))
-  :config
-  (org-roam-setup))
+  :init
+  (org-roam-db-autosync-mode))
